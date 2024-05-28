8e8a71c88d77f6dbc69324c7ad12be967615bce9
karthi S <karthis668@gmail.com>
2024-05-28 09:42:35 +0530

    Autoplaying carousels added

--- src/pages/carouselBanner.jsx
+++ src/pages/carouselBanner.jsx
@@ -1,15 +1,15 @@
 function CarouselBanner() {
   return (
 
-<div id="carouselExampleRide" className="carousel slide" data-bs-ride="true">
+<div id="carouselExampleRide" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
-    <div className="carousel-item active">
+    <div className="carousel-item active" data-bs-interval="1000">
     <img src={'src/assets/banner1.jpg'} className="d-block w-100 banner-img" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-bs-interval="2000">
     <img src={'src/assets/banner2.jpg'}  className="d-block w-100 banner-img" alt="..." />
     </div>
-    <div className="carousel-item">
+    <div className="carousel-item" data-bs-interval="2000">
     <img src={'src/assets/banner3.avif'} className="d-block w-100 banner-img" alt="..." />
     </div>
   </div>
