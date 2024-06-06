8db075af02fd5eeffcc1d4c123bfbf1e2974623a
karthi S <karthis668@gmail.com>
2024-06-06 21:54:26 +0530

    image path changed

--- src/pages/carouselBanner.jsx
+++ src/pages/carouselBanner.jsx
@@ -4,13 +4,13 @@ function CarouselBanner() {
 <div id="carouselExampleRide" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="1000">
-    <img src={'./src/assets/banner1.jpg'} className="d-block w-100 banner-img" alt="..." />
+    <img src='./src/assets/banner1.jpg' className="d-block w-100 banner-img" alt="..." />
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-    <img src={'./src/assets/banner2.jpg'}  className="d-block w-100 banner-img" alt="..." />
+    <img src='./src/assets/banner2.jpg'  className="d-block w-100 banner-img" alt="..." />
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-    <img src={'./src/assets/banner3.avif'} className="d-block w-100 banner-img" alt="..." />
+    <img src='./src/assets/banner3.avif' className="d-block w-100 banner-img" alt="..." />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
